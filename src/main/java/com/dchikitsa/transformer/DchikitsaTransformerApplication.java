c3bf19aa71124ea10a0d8ce3c260298f369a30ee
vedprakash1510 <87705515+vedprakash1510@users.noreply.github.com>
2022-10-18 13:46:04 +0530

    initial commit

---
+++ src/main/java/com/dchikitsa/transformer/DchikitsaTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DchikitsaTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DchikitsaTransformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dchikitsa/transformer/DchikitsaTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchikitsa.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DchikitsaTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
